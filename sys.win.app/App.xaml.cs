3b6c34408f7529a86186a6b03718428fa087003f
Беляк Игорь (Belyak_IS) <Belyak_IS@directum.ru>
2018-06-05 08:34:14 +0400

    Initial

---
+++ sys.win.app/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Mime;
+using System.Windows;
+
+namespace sys.win.app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        
+    }
+}
